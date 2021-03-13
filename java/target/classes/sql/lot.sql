a9afd7d1fd5e5686170dac34dee3e51c5ef5f012
nnocturnnn <vikchehovich@gmail.com>
2021-03-13 23:02:30 +0200

    full

--- java/target/classes/sql/lot.sql
+++ java/target/classes/sql/lot.sql
@@ -29,6 +29,10 @@ CREATE TABLE `Lot` (
   `bidStep` double NOT NULL,
   `duration` int NOT NULL,
   `description` text,
+  `category` text,
+  `start_time` int NOT NULL,
+  `finish_time` int NOT NULL,
+  `active` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,20 +40,3 @@ CREATE TABLE `Lot` (
 --
 -- Dumping data for table `User`
 --
-
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (11,'noisyboy2000@gmail.com','785885',100,'seller'),(12,'dan','5656',100,'seller');
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-02-02 23:29:25
